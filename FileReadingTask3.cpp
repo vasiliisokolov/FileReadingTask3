807cefa88c8022d9a2755a3a05ac035c968d79e0
VasiliiSokolov <vasilysokolov@mail.ru>
2022-11-17 20:55:57 +0300

    Добавьте файлы проекта.

---
+++ FileReadingTask3.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+
+int main()
+{
+    std::vector<char> name, surname;
+    std::vector<int> sum;
+    std::ifstream statement;
+    statement.open("C:\\Users\\Василий\\source\\repos\\FileReadingTask3\\Statement.txt");
+    if (statement.is_open())
+    {
+        std::cout << "Working!" << std::endl;
+        int count = 0;
+        //while (!statement.eof())
+        //{
+
+            statement >> name[count];
+            statement >> surname[count];
+            statement >> sum[count];
+        //}
+        statement.close();
+        std::cout << name[count];
+    }
+    else
+    {
+        std::cout << "File not found!" << std::endl;
+    }
+}

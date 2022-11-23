88b3582e1725bc9e81a84b1514d0672d1a8bb794
VasiliiSokolov <vasilysokolov@mail.ru>
2022-11-23 21:51:02 +0300

    Done!

--- FileReadingTask3.cpp
+++ FileReadingTask3.cpp
@@ -1,55 +1,43 @@
 ﻿#include <iostream>
 #include <fstream>
-#include <vector>
 
 int main()
 {
-    //char buffer[30];
-    char temp;
-    std::string name = "", surname = "", sum ="", date = "";
-    int maxSum = 0;
+    std::string name = "", surname = "", date = "";
+    std::string nameMax = "", surnameMax = "", dateMax = "";
+    int sum, totalSum = 0, sumMax = 0;
     std::ifstream statement;
-    statement.open("C:\\Users\\Василий\\source\\repos\\FileReadingTask3\\Statement.txt", std::ios::binary);
+    statement.open("C:\\Users\\Василий\\source\\repos\\FileReadingTask3\\Statement.txt", std::ios::in);
     if (statement.is_open())
     {
         std::cout << "Working!" << std::endl;
-        
-        
-        while(!statement.eof())
+
+        while (!statement.eof())
         {
-            int turn = 0;
+            statement >> name;
+            statement >> surname;
+            statement >> sum;
+            statement >> date;
 
-            statement.get(temp);
-            if (temp != ' ')
+            if (sum > sumMax)
             {
-                switch (turn)
-                {
-                    case 0: name += temp;
-                        break;
-                    case 1: surname += temp;
-                        break;
-                    case 2: sum += temp;
-                        break;
-                    case 3: date += temp;
-                        break;
+                sumMax = sum;
+                nameMax = name;
+                surnameMax = surname;
+                dateMax = date;
+            }
+            totalSum += sum;
 
-                }
-            }
-            else
-            {
-                turn++;
-            }
-                
-                
-            
-            
         }
-        
+
         statement.close();
-        
+
     }
     else
     {
         std::cout << "File not found!" << std::endl;
     }
+    std::cout << nameMax << " " << surnameMax << " received the maximum payout!" << std::endl;
+    std::cout << "It amounted to " << sumMax << std::endl;
+    std::cout << "Total payout is: " << totalSum << std::endl;
 }
